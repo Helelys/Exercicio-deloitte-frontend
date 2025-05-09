16492eb12f6130b7842a48256b0530b9b9704ed1
Exercicio-Deloitte <tallyslabanca20@gmail.com>
2025-05-09 17:42:15 -0300

    teste

--- Aula01/src/script.js
+++
@@ -1,9 +0,0 @@
-function botaoClicado() {
-    window.alert("Ola")
-}
-
-function botaoFormulario() {
-    window.alert("Login efetuado!")
-    event.preventDefault();
-    console.log("Usuario logado com sucesso!")
-}
